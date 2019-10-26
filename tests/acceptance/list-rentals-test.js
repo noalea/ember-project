9eeafda4054613ac6bc87e59e4ff8d13670e9531
Noa Lea <noagorelik@yahoo.ca>
2019-10-26 11:56:23 -0400

    added listing model to be used with ember data and mirage

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,9 +1,11 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   test('should show rentals as the home page', async function (assert) {
     await visit("/");
